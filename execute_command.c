e51075c796593b7ef3fbc335eeeebe05d08b151a
Agil Mammadov <mammadovagil@tutamail.com>
2026-02-13 22:23:02 +0400

    feat: implement PATH

--- execute_command.c
+++ execute_command.c
@@ -12,16 +12,34 @@ int execute_command(char **args, char **envp, char **argv)
 {
 	pid_t child_pid;
 	int status;
+	char *path = NULL;
 
-	if (args[0] == NULL)
+
+	if (args == NULL || args[0] == NULL)
 		return (1);
+
+	if (args[0][0] == '/' || args[0][0] == '.')
+	{
+		if (is_valid_command(args[0]))
+			path = strdup(args[0]);
+	}
+	else
+		path = get_command_path(args[0]);
+
+
+	if (path == NULL)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
+		return (1);
+	}
 
 	child_pid = fork();
 	if (child_pid == 0)
 	{
-		if (execve(args[0], args, envp) == -1)
+		if (execve(path, args, envp) == -1)
 		{
-			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+			perror(argv[0]);
+			free(path);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -30,6 +48,65 @@ int execute_command(char **args, char **envp, char **argv)
 	else
 		wait(&status);
 
+	free(path);
 	return (1);
 }
 
+/**
+ * get_command_path - checks for a command in PATH variable
+ * @command: command to look up
+ *
+ * Return: path if found, otherwise NULL
+ */
+char *get_command_path(char *command)
+{
+	const char *path_env = getenv("PATH");
+	char *path, *token, *full_path;
+
+	if (!path_env || !command)
+		return (NULL);
+
+	path = strdup(path_env);
+	token = strtok(path, ":");
+
+	while (token != NULL)
+	{
+		/* allocate space: token len + '/' + command len + null terminator */
+		full_path = malloc(strlen(token) + strlen(command) + 2);
+		if (!full_path)
+			break;
+
+		sprintf(full_path, "%s/%s", token, command);
+
+		if (is_valid_command(full_path))
+		{
+			free(path);
+			return (full_path);
+		}
+
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+
+	free(path);
+	return (NULL);
+}
+
+/**
+ * is_valid_command - checks for the path if it is a valid command or not
+ * @path: path to check
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int is_valid_command(char *path)
+{
+	struct stat st;
+
+	if (path == NULL)
+		return (0);
+
+	if (stat(path, &st) == 0 && (st.st_mode & S_IXUSR))
+		return (1);
+
+	return (0);
+}
--- simple_shell.h
+++ simple_shell.h
@@ -6,11 +6,14 @@
 #include <stddef.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 char *read_line(void);
 char **split_line(char *line);
 int execute_command(char **args, char **envp, char **argv);
+char *get_command_path(char *command);
+int is_valid_command(char *command);
 void free_args(char **args);
 
 #endif /* SIMPLE_SHELL_H */
